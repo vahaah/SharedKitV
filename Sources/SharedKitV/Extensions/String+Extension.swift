48374cf4ed45f6601a0604dbc1d3e65621931810
Alex Vakhitov <alex.vakhitov@gmail.com>
2024-07-25 13:42:16 +0100

    fixed apply

--- Sources/SharedKitV/Extensions/String+Extension.swift
+++ Sources/SharedKitV/Extensions/String+Extension.swift
@@ -1,29 +1,31 @@
 import Foundation
 
-extension Optional where Wrapped == String {
-    public func apply(with patch: String?) -> String {
-        guard let self = self else { return patch ?? "" }
-        return self + (patch ?? "")
+extension String? {
+
+    public func apply(with patch: String?) -> String? {
+        if let existing = self {
+            return existing + (patch ?? "")
+        } else {
+            return patch
+        }
     }
 }
 
 extension String {
+
     public func apply(with patch: String?) -> String {
-        self + (patch ?? "")
+        guard let patch else { return self }
+        return self + patch
     }
 
-    public func apply(context: [String: CustomStringConvertible]) -> String {
-        reduce(into: self) { result, char in
-            if char == "{" {
-                if let endIndex = result.firstIndex(of: "}"),
-                   let key = String(result[result.index(after: char.startIndex)..<endIndex]),
-                   let value = context[key]?.description {
-                    result.removeSubrange(char.startIndex...endIndex)
-                    result.insert(contentsOf: value, at: char.startIndex)
-                }
-            }
+    public func apply(context: [String: any StringProtocol]) -> String {
+        var out = self
+        for (key, value) in context {
+            out = out.replacingOccurrences(of: "{\(key)}", with: value)
         }
+        return out
     }
-
-    public static var id: String { UUID().uuidString }
+    public static var id: String {
+        UUID().uuidString
+    }
 }
